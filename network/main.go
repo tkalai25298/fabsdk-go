26068c1a0dfae94b546a33033461bd485112ed3e
kalaiyarasi-ngpws <kalaiyarasi@ngpwebsmart.com>
2021-11-23 20:10:00 +0530

    created artifacts using struct and joined channel

--- network/main.go
+++ network/main.go
@@ -3,43 +3,135 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
+	"path/filepath"
 
 	"github.com/hyperledger/fabric-sdk-go/pkg/client/resmgmt"
 	"github.com/hyperledger/fabric-sdk-go/pkg/core/config"
 
+	"github.com/hyperledger/fabric-sdk-go/pkg/fab/resource"
 	"github.com/hyperledger/fabric-sdk-go/pkg/fab/resource/genesisconfig"
 	"github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
-	"gopkg.in/yaml.v2"
 )
 
-type Configtx struct {
-	Organizations	genesisconfig.Organization
-	Capabilities	Capabilities
-	Application		genesisconfig.Application
-	Orderer			genesisconfig.Orderer
-	Channel			Channel
-	Profiles		genesisconfig.Profile
+func channelCapabilities() map[string]bool {
+	return map[string]bool{
+		"V2_0": true,
+	}
 }
 
-type Capabilities struct {
-	Channel map[string]bool
-	Orderer map[string]bool
-	Application map[string]bool
+func channelDefaults() (map[string]*genesisconfig.Policy, map[string]bool) {
+
+	policies := map[string]*genesisconfig.Policy{
+		"Admins": {
+			Type: "ImplicitMeta",
+			Rule: "ANY Admins",
+		},
+		"Readers": {
+			Type: "ImplicitMeta",
+			Rule: "ANY Readers",
+		},
+		"Writers": {
+			Type: "ImplicitMeta",
+			Rule: "ANY Writers",
+		},
+	}
+	return policies, channelCapabilities()
 }
 
-type Channel struct {
-	Policies	[]map[string]genesisconfig.Policy
-	Capabilities Capabilities
+
+func applicationDefaults() *genesisconfig.Application {
+
+	_, capabilities := channelDefaults()
+
+	return &genesisconfig.Application{
+		Organizations: []*genesisconfig.Organization{},
+		Policies: map[string]*genesisconfig.Policy{
+			"LifecycleEndorsement": {
+				Type: "ImplicitMeta",
+				Rule: "ANY Endorsement",
+			},
+			"Endorsement": {
+				Type: "ImplicitMeta",
+				Rule: "ANY Endorsement",
+			},
+			"Readers": {
+				Type: "ImplicitMeta",
+				Rule: "ANY Readers",
+			},
+			"Writers": {
+				Type: "ImplicitMeta",
+				Rule: "ANY Writers",
+			},
+			"Admins": {
+				Type: "ImplicitMeta",
+				Rule: "ANY Admins",
+			},
+		},
+		Capabilities: capabilities,
+	}
 }
+
+func ngpPolicies() map[string]*genesisconfig.Policy {
+	return map[string]*genesisconfig.Policy{
+		"Readers": {
+			Type: "Signature",
+			Rule: "OR('ngpMSP.admin', 'ngpMSP.peer', 'ngpMSP.client')",
+		},
+		"Writers": {
+			Type: "Signature",
+			Rule: "OR('ngpMSP.admin', 'ngpMSP.client')",
+		},
+		"Admins": {
+			Type: "Signature",
+			Rule: "OR('ngpMSP.admin')",
+		},
+		"Endorsement": {
+			Type: "Signature",
+			Rule: "OR('ngpMSP.peer','ngpMSP.admin')",
+		},
+	}
+}
+
+func ngpOrg() *genesisconfig.Organization {
+	return &genesisconfig.Organization{
+		Name:          "ngpMSP",
+		ID:            "ngpMSP",
+		MSPDir:        filepath.Join("/opt/gopath/src/github.com/hyperledger/fabric/peer", "crypto-config/peerOrganizations/ngp.cpu-network.com/msp"),
+		MSPType:       "bccsp",
+		Policies:      ngpPolicies(),
+		AnchorPeers: []*genesisconfig.AnchorPeer{
+			{
+				Host: "peer0.ngp.cpu-network.com",
+				Port: 7051,
+			},
+		},
+	}
+}
+
+
+func sampleSingleMSPChannel() *genesisconfig.Profile {
+
+	policies, _ := channelDefaults()
+	appDefaults := applicationDefaults()
+	appDefaults.Organizations = []*genesisconfig.Organization{
+		ngpOrg(),
+	}
+
+	return &genesisconfig.Profile{
+		Policies:    policies,
+		Consortium:  "cpuConsortium",
+		Application: appDefaults,
+	}
+}
+
 
 func main() {
 
-	// base := flag.String("ccp", "ccp/cli-ccp.yaml", "The ccp path to use.")
+	base := flag.String("ccp", "ccp/cli-ccp.yaml", "The ccp path to use.")
 	asLocalhost := flag.Bool("localhost", false, "To set weather we want to set localhost.")
 	channelName := "ngpchannel"
-	// configPath	:= "/opt/gopath/src/github.com/hyperledger/fabric/peer/channel-artifacts/channel.tx"
+	configPath	:= "/opt/gopath/src/github.com/hyperledger/fabric/peer/channel-artifacts/channel.tx"
 	flag.Parse()
 
 	if *asLocalhost {
@@ -53,26 +145,31 @@ func main() {
 		}
 	}
 
-	// Create_Join_Channel(base,channelName,configPath)
+	Create_Join_Channel(base,channelName,configPath)
 	CreateArtifacts(channelName)
 }
 
 func CreateArtifacts(channelID string) {
+	config := sampleSingleMSPChannel()
 
-	content, err := ioutil.ReadFile("configtx.yaml")
+	configtx, err := resource.CreateChannelCreateTx(config, nil, channelID)
+
 	if err != nil {
-		fmt.Printf("reading configtx file err: %v",err)
-		os.Exit(1)
-	}
-	
-	configtx := Configtx{}
-	err = yaml.Unmarshal([]byte(content),configtx)
-	if err != nil {
-		fmt.Printf("Unmarshalling configtx.yaml failed: %v",err)
+		fmt.Printf("The error while creating channel tx : %v \n", err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("successfully Unmarshalled configtx \n",configtx.Profiles.Orderer.Addresses)
+	f,err := os.Create("channel-artifacts/channel.tx")
+	if err != nil {
+		fmt.Printf("The error while creating channel tx file: %v \n", err)
+		os.Exit(1)
+	}
+
+	_,err = f.Write(configtx)
+	if err != nil {
+		fmt.Printf("The error while writing channel tx file: %v \n", err)
+		os.Exit(1)
+	}
 }
 
 func Create_Join_Channel(base *string,channelName string,configPath string) {
